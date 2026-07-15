5702f8f1f8df3645192a70f22fb4a11e7da7d6df
Rina <asphyxia1024@gmail.com>
2026-07-15 21:25:03 +0800

    Add mechanics loader and triggered pipelines

---
+++ src/main/java/com/ataraxia/atxcore/mechanic/config/MechanicConfigException.java
@@ -0,0 +1,7 @@
+package com.ataraxia.atxcore.mechanic.config;
+
+public final class MechanicConfigException extends RuntimeException {
+    public MechanicConfigException(String message) {
+        super(message);
+    }
+}
