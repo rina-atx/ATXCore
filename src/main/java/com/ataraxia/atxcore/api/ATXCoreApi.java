441adf193023789784d826aa80865cf645afd352
Rina <asphyxia1024@gmail.com>
2026-07-15 21:23:02 +0800

    Update mechanics and tutorials

--- src/main/java/com/ataraxia/atxcore/api/ATXCoreApi.java
+++ src/main/java/com/ataraxia/atxcore/api/ATXCoreApi.java
@@ -2,6 +2,7 @@ package com.ataraxia.atxcore.api;
 
 import com.ataraxia.atxcore.api.registry.CoreRegistries;
 import com.ataraxia.atxcore.integration.IntegrationManager;
+import com.ataraxia.atxcore.mechanic.config.EcoStyleMechanicLoader;
 import com.ataraxia.atxcore.placeholder.PlaceholderService;
 
 public interface ATXCoreApi {
@@ -10,4 +11,6 @@ public interface ATXCoreApi {
     PlaceholderService placeholders();
 
     IntegrationManager integrations();
+
+    EcoStyleMechanicLoader mechanicLoader();
 }
